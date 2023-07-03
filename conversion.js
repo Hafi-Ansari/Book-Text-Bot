75a559a3176f171d39cde9f60acb205e02fac6f3
hafi-ansari <hafi.m.ansari@gmail.com>
2023-07-03 14:23:42 -0500

    Deciding to use python ebooklib and pymongo for this since js epub deprecated

--- conversion.js
+++ conversion.js
@@ -1,15 +1,13 @@
-const MongoClient = require("mongodb").MongoClient;
-const EPUBParser = require("epub-parser");
 const dotenv = require("dotenv")
-const fs = require("fs");
 
 //config env file
 dotenv.config()
+
+//epub name
+const book = "Red-Rising"
 
 // Connection URL
 const url = process.env.MONGO_URL;
 const dbName = "Red-Rising-Series";
 const collectionName = "Books";
 
-// Create a new MongoClient
-const client = new MongoClient(url);
