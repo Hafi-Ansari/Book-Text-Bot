430fda25823e488374aeceb4d34149706f1bd2d4
hafi-ansari <hafi.m.ansari@gmail.com>
2023-07-04 20:45:47 -0500

    Match vs near match fixed

--- Bot/src/database.js
+++ Bot/src/database.js
@@ -1,33 +1,57 @@
-require('dotenv').config({ path: '../.env' });
-const { Client } = require('@elastic/elasticsearch')
+require("dotenv").config({ path: "../.env" });
+const { Client } = require("@elastic/elasticsearch");
 
 const client = new Client({
   cloud: {
-    id: process.env.ELASTIC_CLOUD_ID
- },
+    id: process.env.ELASTIC_CLOUD_ID,
+  },
   auth: {
-    username: 'elastic',
-    password: process.env.ELASTIC_PASSWORD
-  }
-})
+    username: "elastic",
+    password: process.env.ELASTIC_PASSWORD,
+  },
+});
 
 async function search(query) {
-  const { body } = await client.search({
-    index: 'search-text', // The name of your Elasticsearch index
-    body: {
-      query: {
-        match: { text: query } // Assuming 'text' is the field you want to search
-      }
+    // Attempt a match_phrase query first
+    let { body } = await client.search({
+      index: "new-search-text",
+      body: {
+        query: {
+          match_phrase: { text: query }, // This will look for the exact phrase in the 'text' field
+        },
+      },
+    });
+  
+    let results = ["Exact Matches:", body.hits.hits];
+  
+    // If no results were found with the match_phrase query, perform a fuzzy match query
+    if (results[1].length === 0) {
+      const { body } = await client.search({
+        index: "new-search-text",
+        body: {
+          query: {
+            match: {
+              text: {
+                query: query,
+                fuzziness: "AUTO", // This will look for fuzzy matches in the 'text' field
+              },
+            },
+          },
+        },
+      });
+  
+      results = ["Couldn't Find Exact Matches. Here are some similar results:", body.hits.hits];
     }
-  })
-
-  return body.hits.hits; // This will be an array of search results
-}
-
-// Now you can use the search function like this:
-search('HellDiver')
-  .then(results => {
-    // Do something with the search results
-    console.log(results);
-  })
-  .catch(console.log);
+  
+    return results;
+  }
+  
+  // Now you can use the search function like this:
+  search("HellDiver")
+    .then((results) => {
+      // Do something with the search results
+      console.log(results[0]);
+      console.log(results[1])
+    })
+    .catch(console.log);
+  
---
+++ Bot/src/reindex.js
@@ -0,0 +1,58 @@
+require("dotenv").config({ path: "../.env" });
+const { Client } = require("@elastic/elasticsearch");
+
+const client = new Client({
+  cloud: {
+    id: process.env.ELASTIC_CLOUD_ID,
+  },
+  auth: {
+    username: "elastic",
+    password: process.env.ELASTIC_PASSWORD,
+  },
+});
+
+async function createIndexWithNewMapping() {
+  try {
+    await client.indices.create({
+      index: 'new-search-text',
+      body: {
+        mappings: {
+          properties: {
+            text: {
+              type: 'text',
+              index_options: 'positions'
+            }
+          }
+        }
+      }
+    });
+    console.log('New index created with updated mapping');
+  } catch (error) {
+    console.error('Error creating new index:', error);
+  }
+}
+
+async function reindexData() {
+  try {
+    await client.reindex({
+      body: {
+        source: {
+          index: 'search-text'
+        },
+        dest: {
+          index: 'new-search-text'
+        }
+      }
+    });
+    console.log('Data reindexed successfully');
+  } catch (error) {
+    console.error('Error reindexing data:', error);
+  }
+}
+
+async function updateAndReindex() {
+  await createIndexWithNewMapping();
+  await reindexData();
+}
+
+updateAndReindex();
